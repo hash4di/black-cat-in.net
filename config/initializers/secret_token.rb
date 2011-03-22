b57f3d46808516482bf5a0281fd3a92f7a2f419f
hash4di <hash4di@doubledrones.com>
2011-03-22 10:49:55 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlackCatInNet::Application.config.secret_token = '316d3851b53b08242e8eca0dcda5959aba609d1d36b04cbc3e4fa91ecb73129b93325a7dbd5b3f61ede88d9d3097c1c9e8cc943cf6a6b8b23ebfb9fffac84b47'
