b62d2e32f95a992a94a7b367bf2127db85865331
hash4di <hash4di@doubledrones.com>
2011-03-25 07:51:18 +0100

    fix naming of div for grid960

--- features/step_definitions/application.rb
+++ features/step_definitions/application.rb
@@ -1,9 +1,9 @@
 Then /^I should see Black Cat Logo Image$/ do
-    page.should have_xpath("//img[@id='logo']")
+    page.should have_xpath("//div[@id='logo_image']/img[@id='logo']")
 end
 
 Then /^I should see Black Cat Name$/ do
-  page.should have_xpath("//span[@id='name'][contains(text(), \"Black Cat\")]")
+  page.should have_xpath("//div[@id='name']/span[contains(text(), \"Black Cat\")]")
 end
 
 Then /^I should see Header$/ do
