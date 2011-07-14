c5462fedb60d28c85bb983c7c2877a150e6e5d2b
hash4di <hash4di@doubledrones.com>
2011-07-14 13:50:22 +0200

    fix#3

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 BlackCatInNet::Application.initialize!
-Sass::Plugin.options[:template_location] = { 'app/stylesheets' => 'tmp/stylesheets' }
+#Sass::Plugin.options[:template_location] = { 'app/stylesheets' => 'tmp/stylesheets' }
