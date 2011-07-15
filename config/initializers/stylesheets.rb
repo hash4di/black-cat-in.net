943993dcb67531d1c200297329529c40f82b0ab0
hash4di <hash4di@doubledrones.com>
2011-07-15 11:10:22 +0200

    fix#6

--- config/initializers/stylesheets.rb
+++ config/initializers/stylesheets.rb
@@ -14,7 +14,7 @@ Sass::Plugin.on_updating_stylesheet do |template, css|
   puts "Compiling #{template} to #{css}"
 end
 
-Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
-                                             :urls => ['/stylesheets'],
-                                             :root => "#{Rails.root}/tmp")
+#Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
+#                                             :urls => ['/stylesheets'],
+#                                             :root => "#{Rails.root}/tmp")
 
