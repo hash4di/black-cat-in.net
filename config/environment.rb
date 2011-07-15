fbb92d6854a2e6ee834b944a9da81777dfdbe931
hash4di <hash4di@doubledrones.com>
2011-07-15 12:03:57 +0200

    Cleanup config files due to sass problems.

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 BlackCatInNet::Application.initialize!
-Sass::Plugin.options[:template_location] = { 'app/stylesheets' => 'tmp/stylesheets' }
--- config/initializers/stylesheets.rb
+++ config/initializers/stylesheets.rb
@@ -7,12 +7,13 @@
 # and other posts.
 
 # Create the dir
-require 'fileutils'
-FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
+# XXX
+#require 'fileutils'
+#FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
 
-Sass::Plugin.on_updating_stylesheet do |template, css|
-  puts "Compiling #{template} to #{css}"
-end
+#Sass::Plugin.on_updating_stylesheet do |template, css|
+  #puts "Compiling #{template} to #{css}"
+#end
 
 #Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
 #                                             :urls => ['/stylesheets'],
