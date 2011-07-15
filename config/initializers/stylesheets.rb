6c9385f9ce09324c0e9c3bdeefc514bb25bb991e
hash4di <hash4di@doubledrones.com>
2011-07-15 12:29:41 +0200

    launch plugin in stylesheets.rb

--- config/initializers/stylesheets.rb
+++ config/initializers/stylesheets.rb
@@ -7,13 +7,13 @@
 # and other posts.
 
 # Create the dir
-# XXX
-#require 'fileutils'
-#FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
 
-#Sass::Plugin.on_updating_stylesheet do |template, css|
-  #puts "Compiling #{template} to #{css}"
-#end
+require 'fileutils'
+FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
+
+Sass::Plugin.on_updating_stylesheet do |template, css|
+  puts "Compiling #{template} to #{css}"
+end
 
 #Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
 #                                             :urls => ['/stylesheets'],
