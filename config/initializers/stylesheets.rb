139376964a318640a1bb300460aa47782dfd61f6
hash4di <hash4di@doubledrones.com>
2011-07-16 08:18:59 +0200

    use gist 428746

--- config/initializers/stylesheets.rb
+++ config/initializers/stylesheets.rb
@@ -3,8 +3,7 @@ require 'ninesixty'
 project_type = :rails
 project_path = RAILS_ROOT if defined?(RAILS_ROOT)
 
-http_path    = '/'
-css_dir      = 'tmp/stylesheets'
-sass_dir     = 'app/stylesheets'
-
+http_path = "/"
+css_dir = "tmp/stylesheets"
+sass_dir = "app/stylesheets"
 environment = Compass::AppIntegration::Rails.env
